4d3b8bfc4fc40ebbdb89af30f5b629c8feb0c767
arunachalam <mailtochalam@gmail.com>
2022-03-18 18:57:02 +0530

    slick slider,side nav bar

--- js/custom.js
+++ js/custom.js
@@ -13,192 +13,181 @@
 
 ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+$(document).ready(function () {
+  $(".items").slick({
+    arrows: true,
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
 
-	/* 
+  ("use strict");
+
+  /* 
 
 	1. Vars and Inits
 
 	*/
 
-	var header = $('.header');
-	var hamb = $('.hamburger');
-	var hambActive = false;
-	var menuActive = false;
+  var header = $(".header");
+  var hamb = $(".hamburger");
+  var hambActive = false;
+  var menuActive = false;
 
-	setHeader();
+  setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+  $(window).on("resize", function () {
+    setHeader();
+  });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $(document).on("scroll", function () {
+    setHeader();
+  });
 
-	initMenu();
-	initHomeSlider();
-	initSvg();
+  initMenu();
+  initHomeSlider();
+  initSvg();
 
-	/* 
+  /* 
 
 	2. Set Header
 
 	*/
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 100)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+  function setHeader() {
+    if ($(window).scrollTop() > 100) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  }
 
-	/* 
+  /* 
 
 	3. Init Menu
 
 	*/
 
-	function initMenu()
-	{
-		if($('.hamburger').length)
-		{
-			var hamb = $('.hamburger');
+  function initMenu() {
+    if ($(".hamburger").length) {
+      var hamb = $(".hamburger");
 
-			hamb.on('click', function(event)
-			{
-				event.stopPropagation();
+      hamb.on("click", function (event) {
+        event.stopPropagation();
 
-				if(!menuActive)
-				{
-					openMenu();
-					
-					$(document).one('click', function cls(e)
-					{
-						if($(e.target).hasClass('menu_mm'))
-						{
-							$(document).one('click', cls);
-						}
-						else
-						{
-							closeMenu();
-						}
-					});
-				}
-				else
-				{
-					$('.menu_container').removeClass('active');
-					menuActive = false;
-				}
-			});
-		}
-	}
+        if (!menuActive) {
+          openMenu();
 
-	function openMenu()
-	{
-		var fs = $('.menu_container');
-		fs.addClass('active');
-		hambActive = true;
-		menuActive = true;
-	}
+          $(document).one("click", function cls(e) {
+            if ($(e.target).hasClass("menu_mm")) {
+              $(document).one("click", cls);
+            } else {
+              closeMenu();
+            }
+          });
+        } else {
+          $(".menu_container").removeClass("active");
+          menuActive = false;
+        }
+      });
+    }
+  }
 
-	function closeMenu()
-	{
-		var fs = $('.menu_container');
-		fs.removeClass('active');
-		hambActive = false;
-		menuActive = false;
-	}
+  function openMenu() {
+    var fs = $(".menu_container");
+    fs.addClass("active");
+    hambActive = true;
+    menuActive = true;
+  }
 
-	/* 
+  function closeMenu() {
+    var fs = $(".menu_container");
+    fs.removeClass("active");
+    hambActive = false;
+    menuActive = false;
+  }
+
+  /* 
 
 	2. Init Home Slider
 
 	*/
 
-	function initHomeSlider()
-	{
-		if($('.home_slider').length)
-		{
-			var homeSlider = $('.home_slider');
-			homeSlider.owlCarousel(
-			{
-				items:1,
-				loop:true,
-				autoplay:false,
-				autoplayTimeout:10000,
-				nav:false,
-				dots:false,
-				smartSpeed:1200,
-				onInitialized:startProgressBar,
-				onTranslate:resetProgressBar,
-				onTranslated:startProgressBar
-			});
+  function initHomeSlider() {
+    if ($(".home_slider").length) {
+      var homeSlider = $(".home_slider");
+      homeSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 10000,
+        nav: false,
+        dots: false,
+        smartSpeed: 1200,
+        onInitialized: startProgressBar,
+        onTranslate: resetProgressBar,
+        onTranslated: startProgressBar,
+      });
 
-			function startProgressBar()
-			{
-				if($('.home_slider_progress').length)
-				{
-					$('.home_slider_progress').css({width:"100%", transition:"width 8500ms"});
-				}
-			}
+      function startProgressBar() {
+        if ($(".home_slider_progress").length) {
+          $(".home_slider_progress").css({
+            width: "100%",
+            transition: "width 8500ms",
+          });
+        }
+      }
 
-			function resetProgressBar()
-			{
-				if($('.home_slider_progress').length)
-				{
-					$('.home_slider_progress').css({width:"0", transition:"width 0s"});
-				}
-			}
-			
-		}
-	}
+      function resetProgressBar() {
+        if ($(".home_slider_progress").length) {
+          $(".home_slider_progress").css({
+            width: "0",
+            transition: "width 0s",
+          });
+        }
+      }
+    }
+  }
 
-	/* 
+  /* 
 
 	6. Init SVG
 
 	*/
 
-	function initSvg()
-	{
-		jQuery('img.svg').each(function()
-		{
-			var $img = jQuery(this);
-			var imgID = $img.attr('id');
-			var imgClass = $img.attr('class');
-			var imgURL = $img.attr('src');
+  function initSvg() {
+    jQuery("img.svg").each(function () {
+      var $img = jQuery(this);
+      var imgID = $img.attr("id");
+      var imgClass = $img.attr("class");
+      var imgURL = $img.attr("src");
 
-			jQuery.get(imgURL, function(data)
-			{
-				// Get the SVG tag, ignore the rest
-				var $svg = jQuery(data).find('svg');
+      jQuery.get(
+        imgURL,
+        function (data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find("svg");
 
-				// Add replaced image's ID to the new SVG
-				if(typeof imgID !== 'undefined') {
-				$svg = $svg.attr('id', imgID);
-				}
-				// Add replaced image's classes to the new SVG
-				if(typeof imgClass !== 'undefined') {
-				$svg = $svg.attr('class', imgClass+' replaced-svg');
-				}
+          // Add replaced image's ID to the new SVG
+          if (typeof imgID !== "undefined") {
+            $svg = $svg.attr("id", imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if (typeof imgClass !== "undefined") {
+            $svg = $svg.attr("class", imgClass + " replaced-svg");
+          }
 
-				// Remove any invalid XML tags as per http://validator.w3.org
-				$svg = $svg.removeAttr('xmlns:a');
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr("xmlns:a");
 
-				// Replace image with new SVG
-				$img.replaceWith($svg);
-			}, 'xml');
-		});
-	}
-
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+        },
+        "xml"
+      );
+    });
+  }
 });
