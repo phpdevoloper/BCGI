29f519c38d59deeb6bc5dab7a17d1d4ee2324060
arunachalam <mailtochalam@gmail.com>
2022-04-21 18:25:42 +0530

    responsive fixed

--- js/custom.js
+++ js/custom.js
@@ -15,9 +15,9 @@
 
 $(document).ready(function () {
   //mouse outside of click event
-  $(".menu_clos").on("click", function (event) {
-    $("#sidebarMenu").removeClass("show");
-  });
+  // $(".menu_clos").on("click", function (event) {
+  //   $("#sidebarMenu").removeClass("show");
+  // });
 
   //footer slick slide
   $(".items").slick({
@@ -28,6 +28,23 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   //photo gallery slider
   $(".gallery").slick({
@@ -38,6 +55,23 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   ("use strict");
@@ -120,8 +154,8 @@ $(document).ready(function () {
   }
 
   function closeMenu() {
-    var fs = $(".sidebar");
-    fs.removeClass("show");
+    var fs = $(".menu_container");
+    fs.removeClass("active");
     hambActive = false;
     menuActive = false;
   }
